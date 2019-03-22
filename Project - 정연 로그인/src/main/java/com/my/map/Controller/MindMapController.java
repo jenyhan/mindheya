a22e2d19d32d10759f8c65b795d04e7946235645
HOE INN KIM <43197755+HOEINNKIM@users.noreply.github.com>
2019-03-22 13:29:13 +0900

    회인

--- Project - 정연 로그인/src/main/java/com/my/map/Controller/MindMapController.java
+++ Project - 정연 로그인/src/main/java/com/my/map/Controller/MindMapController.java
@@ -2,16 +2,25 @@ package com.my.map.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.my.map.vo.MindMap;
+
 @Controller
 public class MindMapController {
-	
+
 	//마인드맵 페이지 이동
 	@RequestMapping(value="/goMindmap", method=RequestMethod.GET)
 	public String goMindmap() {
+		return "selectMind";
+	}
+
+	@RequestMapping(value="/goMap", method=RequestMethod.GET)
+	public String goMap(MindMap mind, Model model) {
+		model.addAttribute("mindMap", mind);
 		return "mindMap";
 	}
-		
+
 }
